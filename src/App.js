8cdd49adeb44981bdc08ba57be598550b9bf1242
Sriram <sri761177@gmail.com>
2023-08-30 00:34:26 +0530

    Add css for flex model btn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './app.css'
+import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
+import Offer from './Components/Offers/Offer'
+import About from './Components/About/About'
+import Blog from './Components/Blog/Blog'
+import Popular from './Components/Popular/Popular'
 
 
 const App = () => {
   return (
-    <div>Working</div>
+    <>
+    <Navbar/>
+    {/*<Home/>
+    <Popular/>
+    <Offer/>
+    <About/>
+    <Blog/>
+  <Footer>*/}
+    </>
   )
 }
 
